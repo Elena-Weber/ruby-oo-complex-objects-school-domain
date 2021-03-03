2cd1d8c1ea2d0a1d152296908dfe43b92732949c
Elena-Weber <crochetmiracles@gmail.com>
2021-03-03 15:28:02 -0500

    commit tree 72c2bcc73396655751321e41defe349c35ab5832

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+attr_accessor :name, :roster
+
+def initialize(name, roster = {})
+@name = name
+@roster = roster
+end
+
+def add_student(student, student_grade)
+    roster[student_grade] ||= []
+    roster[student_grade] << student
+end
+
+def grade(grade)
+    @roster[grade]
+end
+
+def sort
+    sorted_students = {}
+    roster.each do |grade, students|
+        sorted_students[grade] = students.sort
+    end
+    sorted_students
+end
+
+end
